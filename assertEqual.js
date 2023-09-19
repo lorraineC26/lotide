181d96b9618b0b691eca4dfb0edb7586f1744b45
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-19 00:25:13 -0400

    format adjusted

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log("✅Assertion Passed: " + actual + " === " + expected);
   } else {
