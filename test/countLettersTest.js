c4609299fdd86e89a0b6c61913be68c3c1de0b3f
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-20 18:27:43 -0400

    test codes

---
+++ test/countLettersTest.js
@@ -0,0 +1,5 @@
+const testResult = countLetters("lighthouse in the house");
+assertEqual(testResult["l"], 1)
+assertEqual(testResult["i"], 2)
+assertEqual(testResult["h"], 4)
+assertEqual(testResult["z"], undefined)
