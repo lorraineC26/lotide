fc1fe75b74c573779d72e638b5296c47580c3d66
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-20 19:47:27 -0400

    test

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,12 @@
+//test cases
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That Show"), undefined);
