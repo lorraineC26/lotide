d9746dbcb0e545009d2c73c8521a3732c0204c45
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-20 10:03:33 -0400

    test cases

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,4 @@
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 3])
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])
+assertArraysEqual(["1", "3", "5"], ["1", "1", "2"])
