a8aba5fb53599f30b17c6c1779ff3570b5e86d70
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-20 18:04:26 -0400

    return spcific counts as obj based on input array

---
+++ countOnly.js
@@ -0,0 +1,28 @@
+//for testing
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function (allItems, itemsToCount) {
+  const results = {}
+
+  for (const item of allItems) { // iterate all the name listed in the provided array; the name appears only in the obj won't be counted in this way i.e., Karima
+    if (itemsToCount[item]) { // if the name listed in the array is also present in the input object and showing true
+      if (results[item]) { // if the name has already been included in the final result obj
+        results[item] += 1;
+      } else {
+        results[item] = 1; // if the name hasn't yet been added to the final result obj
+      }
+    }
+  }
+  return results;
+}
+
+
+//test case goes below
