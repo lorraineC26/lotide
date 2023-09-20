af3c9c6aa5fd67b7bfaa064049a1f868d6e79be0
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-20 09:45:48 -0400

    compare two arrays

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(arrOne, arrTwo) {
+  let ans = true;
+  for (let index = 0; index < arrOne.length; index++) {
+    if (arrOne[index] !== arrTwo[index]) {
+      ans = false;
+      return ans;
+    }
+  }
+  return ans;
+}
+
+
+
+//test cases:
+
