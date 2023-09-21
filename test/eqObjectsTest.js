c54d1b2331ed286be1c86e76082c61a1d75bf2d4
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-20 20:42:08 -0400

    test

---
+++ test/eqObjectsTest.js
@@ -0,0 +1,10 @@
+//test case #1
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+
+//test case #2
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
