e1a37d2273327d5631d43889b8c17583c222e9ac
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-25 15:41:24 -0400

    module.exports added

--- tail.js
+++ tail.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   return array.slice(1); //creat a new array starting at the index 1 of the previous array
 };
 
-//test code
+module.exports = tail;
+
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,19 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
 //test case 1
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+const result1 = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result1.length, 2); // ensure we get back two elements
+assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
 
 //test case 2
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const words1 = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words1); // no need to capture the return value since we are not checking it
+assertEqual(words1.length, 3); // original array should still have 3 elements!
 
 //test case 3
-const words = ["justMe"];
-const result = tail(words);
-assertEqual(result.length, 0); //An array with only one element should yield an empty array for its tail
-assertEqual(result[0], undefined);
+const words2 = ["justMe"];
+const result2 = tail(words2);
+assertEqual(result2.length, 0); //An array with only one element should yield an empty array for its tail
+assertEqual(result2[0], undefined);
