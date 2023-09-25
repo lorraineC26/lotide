910a65927090b2d89738acfae4a99af628eb4c37
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-25 17:46:02 -0400

    module.exports added

--- middle.js
+++ middle.js
@@ -1,25 +1,5 @@
-function eqArrays(arrOne, arrTwo) {
-  let ans = true;
-  for (let index = 0; index < arrOne.length; index++) {
-    if (arrOne[index] !== arrTwo[index]) {
-      ans = false;
-      return ans;
-    }
-  }
-  return ans;
-}
-
-// assertArraysEqual for testing our new function
-const assertArraysEqual = function (arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅Assertion Passed: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`❌Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
 // return an array showing the element at the middle position
-function middle(array) {  
+const middle = function(array) {
   let middleArray = [];
   if (array.length % 2 !== 0 && array.length > 2) { // array with odd number of elements but not include arrays with one or two elements
     middleArray = array[(array.length - 1) / 2];
@@ -27,5 +7,6 @@ function middle(array) {
     middleArray = array.slice(((array.length / 2) - 1), ((array.length / 2) + 1));
   }
   return middleArray;
-}
+};
 
+module.exports = middle;
